846bb665be6286664248588483ef318840017290
souguxtavo <gustabarbosa6@gmail.com>
2024-03-30 03:21:56 -0300

    modify use in albumcontroller and api

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Album;
 
-class Album extends Controller
+class AlbumController extends Controller
 {
     /**
      * Display a listing of the resource.
