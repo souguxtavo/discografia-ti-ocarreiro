9dcf44eb04a4dba88ea7a1cf57a51a27ed78269f
souguxtavo <gustabarbosa6@gmail.com>
2024-04-02 21:51:48 -0300

    routes web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AlbumController;
+use App\Http\Controllers\FaixaController;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
 require __DIR__.'/auth.php';
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/album', [AlbumController::class, 'index']);
+    Route::post('/album', [AlbumController::class, 'store']); 
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/faixa', [FaixaController::class, 'index']);
+    Route::post('/faixa', [FaixaController::class, 'store']);
+});
+
+
