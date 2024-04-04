dc3d7024ee548232dd0135899e152661c476e411
souguxtavo <gustabarbosa6@gmail.com>
2024-04-04 01:54:40 -0300

    do delete

--- react/src/pages/consultaalbums.tsx
+++ react/src/pages/consultaalbums.tsx
@@ -12,6 +12,21 @@ const ConsultaAlbums = () => {
       .then(data => setAlbuns(data));
   }, []);
 
+  const handleDelete = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:8000/api/album/${id}`, {
+        method: 'DELETE',
+      });
+      if (response.status === 200) {
+        // Remove the album with the specified ID from the list
+        const updatedAlbums = albuns.filter(album => album.id !== id);
+        setAlbuns(updatedAlbums);
+      }
+    } catch (error) {
+      console.error('Erro ao excluir:', error);
+    }
+  };
+
   return (
     <div className="App">
       <div className="container mt-5">
@@ -22,6 +37,8 @@ const ConsultaAlbums = () => {
             <tr>
               <th scope="col">Nome</th>
               <th scope="col">Ano</th>
+              <th scope="col">Visualizar Faixas</th> {/* New column for view tracks button */}
+              <th scope="col">Ações</th>
             </tr>
           </thead>
           <tbody>
@@ -29,6 +46,21 @@ const ConsultaAlbums = () => {
               <tr key={album.id}>
                 <td>{album.nome}</td>
                 <td>{album.ano}</td>
+                <td>
+                  <Link to={`/pages/faixas/${album.id}`}>
+                    <button className="btn btn-primary">
+                      Visualizar
+                    </button>
+                  </Link>
+                </td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => handleDelete(album.id)}
+                  >
+                    Excluir
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
